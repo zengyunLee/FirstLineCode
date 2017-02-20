c96c08d62bbc9de0d09acf1b80653879d88260b6
zengyunLee <feiyu521.ok@163.com>
2017-02-20 19:57:58 +0800

    this is an example to display how to send self-defining standard (or unordered) Broadcast and receive it.

--- BroadcastTest/app/src/main/java/com/lizengyunle/broadcasttest/MainActivity.java
+++ BroadcastTest/app/src/main/java/com/lizengyunle/broadcasttest/MainActivity.java
@@ -8,6 +8,8 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,15 +20,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        intentFilter = new IntentFilter();
-        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
-        networkChangeReceiver = new NetworkChangeReceiver();
-        registerReceiver(networkChangeReceiver,intentFilter);
+
+//        intentFilter = new IntentFilter();
+//        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+//        networkChangeReceiver = new NetworkChangeReceiver();
+//        registerReceiver(networkChangeReceiver,intentFilter);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent("com.lizengyunle.broadcasttest.MY_BROADCAST");
+                sendBroadcast(intent);
+            }
+        });
     }
     @Override
     protected void onDestroy(){
         super.onDestroy();
-        unregisterReceiver(networkChangeReceiver);
+        //unregisterReceiver(networkChangeReceiver);
     }
     class NetworkChangeReceiver extends BroadcastReceiver{
         @Override
